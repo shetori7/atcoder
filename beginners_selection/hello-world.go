c18273499589e74e286e70b332413a060e8beca2
shetori <shetori4818@gmail.com>
2024-12-17 21:55:00 +0900

    AtCoder Beginners Selection

---
+++ beginners_selection/hello-world.go
@@ -0,0 +1,9 @@
+// はじめのプログラムは "hello world" を表示するものだ。
+// ソースコードのここにあるものですべてだ（ソースコードの右上にある Gopher 君をクリックすると、ブラウザ上でプログラムを実行できる）。
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("hello world")
+}
